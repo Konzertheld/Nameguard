73acda0d275e1c3bc5d3adec74e1d00a132fbc32
Konzertheld <christian.gredig@googlemail.com>
2013-01-09 06:03:16 +0100

    Initial commit. Nameguard protects your users from name-abuse in comments.

---
+++ nameguard.plugin.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Nameguard Plugin
+ */
+ 
+class Nameguard extends Plugin
+{
+	/**
+	 * Hook to the comment form to add a validator to the commenter name
+	 */
+	 function action_form_comment($form)
+	 {
+		$form->cf_commenter->add_validator(array($this, 'validate_commenter'));
+	 }
+	 
+	 function validate_commenter($commenter, $control, $form)
+	 {
+		$users = Users::get(array('username' => $commenter));
+		if(!count($users))
+		{
+			$users = Users::get_by_info('displayname', $commenter);
+			if(!count($users))
+			{
+				return array();
+			}
+		}
+		return array(_t('This name is already in use!', __CLASS__));
+	 }
+}
+?>
