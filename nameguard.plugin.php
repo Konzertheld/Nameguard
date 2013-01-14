410fac5e7ed736dcce572f718884bea49a3114f9
Konzertheld <christian.gredig@googlemail.com>
2013-01-14 22:41:06 +0100

    Registered user are of course allowed to comment as themselves.

--- nameguard.plugin.php
+++ nameguard.plugin.php
@@ -15,6 +15,13 @@ class Nameguard extends Plugin
 	 
 	 function validate_commenter($commenter, $control, $form)
 	 {
+		// don't care if the user is logged in and commenting as himself
+		$user = User::identify();
+		if($commenter == $user->displayname || $commenter == $user->username)
+		{
+			return array();
+		}
+		
 		$users = Users::get(array('username' => $commenter));
 		if(!count($users))
 		{
@@ -24,7 +31,7 @@ class Nameguard extends Plugin
 				return array();
 			}
 		}
-		return array(_t('This name is already in use!', __CLASS__));
+		return array(_t('This name is already in use by a registered user!', __CLASS__));
 	 }
 }
 ?>
